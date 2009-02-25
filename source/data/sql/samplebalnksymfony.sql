55ccf1d47f7fe822a4c8772fd6e73e86fb436f0c
gs.bajaj <gs.bajaj@f7779f97-b854-0410-af7b-71d285e187ea>
2009-02-25 11:06:36 +0000

    initial setup with features as mentioned in Issue # 11

---
+++ source/data/sql/samplebalnksymfony.sql
@@ -0,0 +1,32 @@
+/*
+SQLyog - Free MySQL GUI v5.01
+Host - 5.0.22 : Database - stratycon
+*********************************************************************
+Server version : 5.0.22
+*/
+
+
+create database if not exists `stratycon`;
+
+USE `stratycon`;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+/*Data for the table `role` */
+
+insert into `role` values 
+(1,'admin','Can Manage the system'),
+(2,'sales','Marketing dealing '),
+(3,'tech','Technical Assistant');
+
+/*Data for the table `user` */
+
+insert into `user` values 
+(1,'admin','73a301bbf7bf1e3c53419114e29d37d32d5e31d2','admin','ahmermunir@neevtech.com','Bangalore',1,'Ahmer','Munir',0,0);
+
+/*Data for the table `userRole` */
+
+insert into `userRole` values 
+(1,1,1);
+
+SET FOREIGN_KEY_CHECKS=1;
