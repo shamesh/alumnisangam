54b5f33d0fbbe113cc17d1d6cdd4749552b86376
gs.bajaj <gs.bajaj@f7779f97-b854-0410-af7b-71d285e187ea>
2009-03-10 04:35:16 +0000

    BAJAJ - Search module as base for other search based features...

--- source/apps/frontend/modules/home/templates/searchSuccess.php
+++ source/apps/frontend/modules/home/templates/searchSuccess.php
@@ -1,46 +1,48 @@
 <?php include_component('home','leftmenu'); ?>
 
-
+<input type="hidden" id="selectedid" name="selectedid">
+<table border="1">
 <?php if($flag ==1): echo "Select At least one field...";
 	  elseif($results):
 	  foreach($results as $rs):
 ?>
 
-<div>
-<?php echo "Year: ".$rs->getGraduationyear(); ?>
-</div>
+<tr>
+<td><?php echo "Year: ".$rs->getGraduationyear(); ?></td>
 
-<div>
-<?php 
+<td><?php 
 	if($rs->getBranch()){
 	echo "Branch: ".$rs->getBranch()->getName(); 
 	}
-?>
-</div>
-
-<div>
-<?php
+?></td>
+<td><?php
 	if($rs->getDegree()){
 	echo "Degree: ".$rs->getDegree()->getName(); 
 	}
-	?>
-</div>
-
-<div>
-<?php 
+	?></td>
+<!--<td><?php /*
 	$chapter = ChapterPeer::retrieveByPK($chapterid);
 	if($chapter){
 	echo "Chapter: ".$chapter->getName(); 
 	}
-	?>
-</div>
+	else{
+		echo "none";
+	}
+	*/ ?></td>
+--><td><input type="radio" id="<?php echo $rs->getId() ?>" onclick="checkme(<?php echo $rs->getId() ?>)"></td>
 
-<div>
-<?php // echo "Currently At: ".$rs->getCurrentlyat(); ?>
-</div>
 <?php endforeach;
 
 	else: echo "Sorry!! No maching results found...";
 
 	endif;
 ?>
+</table>
+
+
+<script type="text/javascript">
+function checkme(id)
+{
+	document.getElementById('selectedid').value = id;
+}
+</script>
