fc57e90f4151138b20fad9435ef404d91a8c6dc0
gs.bajaj <gs.bajaj@f7779f97-b854-0410-af7b-71d285e187ea>
2009-08-27 06:03:19 +0000

    fixes #77(3)

--- source/apps/frontend/modules/user/templates/welcomeSuccess.php
+++ source/apps/frontend/modules/user/templates/welcomeSuccess.php
@@ -10,4 +10,11 @@
 		<a href="http://192.168.1.209:80/phpBB3/">Forum @ phpBB3</a>
 		
 	</div>
+	<?php if($user): ?>
+		<?php if($user->getIsinvited() == 4): ?>
+			<div class="oddRow">
+			<div>The invitation to be an authorizer is pending <?php echo link_to('Click here to authorize', '/settings/invites') ?> </div>
+			</div>
+		<?php endif; ?>
+	<?php endif; ?>
 </div>
