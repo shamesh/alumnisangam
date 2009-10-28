b9e8896b6b65576d814dd2b1d4f376844a54dbc0
gs.bajaj <gs.bajaj@f7779f97-b854-0410-af7b-71d285e187ea>
2009-10-28 09:21:29 +0000

    resolving inconsistency

---
+++ source/data/symfony/modules/default/templates/unavailableSuccess.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<?php $path = sfConfig::get('sf_relative_url_root', preg_replace('#/[^/]+\.php5?$#', '', isset($_SERVER['SCRIPT_NAME']) ? $_SERVER['SCRIPT_NAME'] : (isset($_SERVER['ORIG_SCRIPT_NAME']) ? $_SERVER['ORIG_SCRIPT_NAME'] : ''))) ?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="title" content="Web Design, Web CMS, Web Hosting, Personalized Domain Name - WEB BAZAAR" />
+<meta name="robots" content="index, follow" />
+<meta name="description" content="Get Online Get Noticed. Web Bazaar builds CMS hosted websites at a very affordable price. We help you create your own personalized website." />
+<meta name="keywords" content="Web Design, Web CMS,Web Hosting,Personalized Domain Name,WEB BAZAAR" />
+<meta name="language" content="en" />
+<title>Web Design, Web CMS, Web Hosting, Personalized Domain Name - WEB BAZAAR</title>
+<link rel="shortcut icon" href="/favicon.ico" />
+<link rel="stylesheet" type="text/css" media="screen" href="/css/modernstyle.css" />
+</head>
+<body>
+<noscript>
+<center>
+<div id="noScript"><img src="/images/icons/iconWarning.gif" /> JavaScript must
+be enabled in order for you to use Web Bazaar.<br />
+However, it seems JavaScript is either disabled or not supported by your
+browser. <br />
+To use Web Bazaar, enable JavaScript by changing your browser options.<br />
+</div>
+</center>
+</noscript>
+<div class="main">
+	<div class="mainTopclient">
+		<div class="innerTop">
+			<div class="header">
+				<div class="headerLeft">
+				<a href="/home/index.html"><img src="/images/modernsite/new_logo.gif" /></a>
+				</div>
+				<div class="headerRight">
+					<div class="headerRightDiv1"><img src="/images/modernsite/callus.gif" /></div>
+					<div class="headerRightDiv2">+91 80 40905650<br />+91 9739973445</div>
+				</div>
+				<div class="headerTopMenu">
+						<ul>
+							<li><a href="admin/login" class='rightSde_linkTxt'>Login</a></li>
+							<li>|</li>
+							<li><?php echo link_to('Buy','domain/domain');?></li>
+							<li>|</li>
+							<li><?php echo link_to('Sitemap','home/sitemap',array('class'=>'rightSde_linkTxt')) ?></li>	
+						</ul>
+				</div>
+			</div>
+			<div class="container" >
+				<div class="containerTop3">
+					<div class="leftNav" style="text-align: center; float: right;">
+						<div  class="leftNavDiv1"><img src="/images/modernsite/unavailable.gif" /></div>
+						<div class="leftNavDiv2">
+						<table width="100%">
+						<tr>
+						<td align="center">
+						<img src="/images/payment/pay3_oops.jpg" />
+						</td>
+						</tr>
+						<tr>
+						<td align="left" height="25">
+						<b>This page may have moved or is no longer available</b>
+						</td>
+						</tr>
+						<tr>
+						<td align="left" height="25">
+						Please try one of the following options:
+						</td>
+						</tr>
+						<tr>
+						<td align="left" style="padding-left: 30px;">
+							<ul>
+								<li>Check the Web address you entered to make sure if it's correct.</li>
+								<li>If you are in Help searching for a Help page, click the Back button to go back to your search results and select another Help page link.</li>
+								<li>Report this technical issue or view known technical issues</li>
+								<li>Site may be temporary unavailable. For more detail you can contact on <a href="mailto:support@webbazaar.com">support@webbazaar.com</a></li>
+							</ul>
+						</td>
+						</tr>
+						<tr>
+						<td><b>&nbsp;</b></td>
+						</tr>
+						<tr>
+						<td><b>What Next:</b></td>
+						</tr>
+						<tr>
+						<td style="padding-left: 30px;" class="containerInnerDiv4Lnk"><img src="/images/site/pay1_red_arrow.jpg" /><?php echo link_to('Go Back to <font color="#4397be">Home Page</font>','home/index') ?></td>
+						</tr>
+						</table>
+					</div>	
+					</div>
+				</div>	
+			</div>
+			<div class="footer">
+				<div class="footerMenuTop">
+				</div>
+				<div class="footerMenuBottom">
+					<ul>
+						<li>Copyright &copy; Web Bazaar All Rights reserved</li>
+						<li>|</li>
+						<li><?php echo link_to('Privacy Policy','home/privacy') ?></li>
+						<li>|</li>
+						<li><?php echo link_to('Terms of Use','home/termofuse') ?></li>
+					</ul>
+				</div>
+			</div>	
+		</div>
+	</div>
+</div>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("UA-3028378-4");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+</body>
+</html>
